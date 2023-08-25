59e14ba051398b762de625330b2a13001e93e595
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-25 23:07:26 +0900

    complete_cpt_5

---
+++ app.js
@@ -0,0 +1,34 @@
+//　上から下へスライドさせる
+// $(function () {
+//   $('.box1').slideDown();
+// });
+
+// 下から上へスライドさせる
+// $(function () {
+//   $('.box1').slideUp();
+// });
+
+// 非表示の要素をjQueryで表示させる
+// $(function () {
+//   $('.box1').show();
+//   $('.box1').css({ 'background-color': '#0000FF' });
+// });
+
+// 表示されている要素をjQueryで非表示にする
+// $(function () {
+//   $('.box1').hide();
+// });
+
+//要素を上から下へスライドさせた後、
+//赤色の正方形を青色の長方形（幅200px、高さ100px）に変更
+$(function () {
+  $('.box1').slideDown(3000, function () {
+    $('.box1')
+      .css({
+        'background-color': '#0000FF',
+        width: '200px',
+        height: '100px',
+      })
+      .slideUp(3000);
+  });
+});
