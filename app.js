caaccbc7d72df7a7b77f75a8d20704d328c9c838
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-28 18:18:00 +0900

    update_cpt_6

--- app.js
+++ app.js
@@ -21,14 +21,23 @@
 
 //要素を上から下へスライドさせた後、
 //赤色の正方形を青色の長方形（幅200px、高さ100px）に変更
+// $(function () {
+//   $('.box1').slideDown(3000, function () {
+//     $('.box1')
+//       .css({
+//         'background-color': '#0000FF',
+//         width: '200px',
+//         height: '100px',
+//       })
+//       .slideUp(3000);
+//   });
+// });
+
 $(function () {
-  $('.box1').slideDown(3000, function () {
-    $('.box1')
-      .css({
-        'background-color': '#0000FF',
-        width: '200px',
-        height: '100px',
-      })
-      .slideUp(3000);
+  $('.box1').mouseover(function () {
+    $('.box1').css({ 'background-color': '#0000FF' });
+  });
+  $('.box1').mouseout(function () {
+    $('.box1').css({ 'background-color': '#FF0000' });
   });
 });
